2a767459db3909b313aa898a07b12efbb1c36d3b
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-10-16 16:50:46 -0400

    compiled todo taskCount in smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0; 
 
 contract TodoList {
-    
+    // Create a state variable to keep track of the number of tasks (these are written to the blockchain)
+    // access keyword "public" allows us to read the value 'taskCount' 
+    uint public taskCount = 0;
 }
