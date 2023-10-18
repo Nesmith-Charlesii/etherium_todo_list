a01866ebbd5525af3a0fb59f18d256085cc324d6
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-10-18 15:55:46 -0400

    project setup

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,5 @@ contract TodoList {
     // Create a state variable to keep track of the number of tasks (these are written to the blockchain)
     // access keyword "public" allows us to read the value 'taskCount' 
     uint public taskCount = 0;
+    
 }
