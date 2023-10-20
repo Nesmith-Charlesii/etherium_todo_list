3d958967d8a887e2278d1fc3fc24de9c384523d5
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-10-20 14:20:57 -0400

    updated smart contract with seeded task and a function to execute the task count

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,29 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     // Create a state variable to keep track of the number of tasks (these are written to the blockchain)
-    // access keyword "public" allows us to read the value 'taskCount' 
+    // access keyword "public" allows us to read the value 'taskCount'
+    // uint is an unsigned integer. It cannot be negative
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // mapping takes a key/value pair. The uint is an id for the Task struct
+    // public allows read access to items from mapping
+    mapping (uint => Task) public tasks; 
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
     
+
 }
